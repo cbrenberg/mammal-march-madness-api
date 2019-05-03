864dedb69c12342d4af320e94e1779796e9574d9
Christopher Brenberg <cbrenberg@gmail.com>
2019-05-03 15:42:47 -0500

    Configure authentication services

---
+++ Domain/Models/Configuration/JWTSettings.cs
@@ -0,0 +1,22 @@
+using Newtonsoft.Json;
+
+namespace MMM_Bracket.API
+{
+  public class JWTSettings
+  {
+    [JsonProperty("secret")]
+    public string SecretKey { get; set; }
+
+    [JsonProperty("issuer")]
+    public string Issuer { get; set; }
+
+    [JsonProperty("audience")]
+    public string Audience { get; set; }
+
+    [JsonProperty("accessExpiration")]
+    public int AccessExpiration { get; set; }
+
+    [JsonProperty("refreshExpiration")]
+    public int RefreshExpiration { get; set; }
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using MMM_Bracket.API.Domain.Repositories;
 using MMM_Bracket.API.Domain.Services;
 using MMM_Bracket.API.Persistence.Contexts;
 using MMM_Bracket.API.Persistence.Repositories;
 using MMM_Bracket.API.Services;
 using AutoMapper;
+using JWT;
 
 namespace MMM_Bracket.API
 {
@@ -33,10 +38,36 @@ namespace MMM_Bracket.API
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      //Configure user-secrets
+      services.Configure<DatabaseSecrets>(Configuration.GetSection("Database"));
+      services.Configure<JWTSettings>(Configuration.GetSection("JWTSettings"));
+      var token = Configuration.GetSection("tokenManagement").Get<JWTSettings>();
+      var secret = Encoding.ASCII.GetBytes(token.SecretKey);
+
+      //configure JWTBearer Authentication
+      services.AddAuthentication(x =>
+      {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(x =>
+      {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(token.SecretKey)),
+          ValidIssuer = token.Issuer,
+          ValidAudience = token.Audience,
+          ValidateIssuer = false,
+          ValidateAudience = false
+        };
+      });
+
       services.AddAutoMapper();
 
       services.AddEntityFrameworkNpgsql().AddDbContext<mmm_bracketContext>().BuildServiceProvider();
-      services.Configure<DatabaseSecrets>(Configuration.GetSection("Database"));
 
       services.AddScoped<IAnimalRepository, AnimalRepository>();
       services.AddScoped<IAnimalService, AnimalService>();
@@ -61,6 +92,8 @@ namespace MMM_Bracket.API
         app.UseHsts();
       }
 
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+      app.UseAuthentication();
       app.UseHttpsRedirection();
       app.UseMvc();
     }
