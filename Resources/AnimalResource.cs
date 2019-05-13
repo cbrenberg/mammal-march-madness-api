c6c30ed6412f22335c0ea236b95dadb94bb5eec4
Christopher Brenberg <cbrenberg@gmail.com>
2019-05-13 16:35:40 -0500

    remove constructor

--- Resources/AnimalResource.cs
+++ Resources/AnimalResource.cs
@@ -5,10 +5,6 @@ namespace MMM_Bracket.API.Resources
 {
   public partial class AnimalResource
   {
-    public AnimalResource()
-    {
-    }
-
     public int Id { get; set; }
     public int CategoryId { get; set; }
     public string Name { get; set; }
